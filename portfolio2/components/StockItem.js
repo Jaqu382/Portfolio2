84abb41e3f66db7d6d162b5d5c6c3a6c01490334
Jaqu382 <ja.juan.aquino.ja@gmail.com>
2022-04-25 20:24:26 -0400

    Nearing full function. Missing dynamic response to user update

--- portfolio2/components/StockItem.js
+++ portfolio2/components/StockItem.js
@@ -1,22 +1,55 @@
 import React, { useCallback, useEffect, useState } from "react";
 import { StyleSheet, Text, View, Image } from "react-native";
 import { Button, Card, CheckBox, Input } from "react-native-elements";
-import Inventory from "../screens/Inventory";
 
 export default function StockItem(props) {
   let { item, inventory, newInventory, setNewInventory } = props;
-  let [newStockItem, setNewStockItem] = useState({});
-  let [changed, setChanged] = useState(false);
-
-  let addStock = useCallback((addedStock) => {
-    setChanged(true);
+  let [newPrice, setNewPrice] = useState();
+  let [newDiscount, setNewDiscount] = useState();
+  let [onSale, setOnSale] = useState(item.sale);
+  const addStock = useCallback((addedStock) => {
     addedStock.qty = addedStock.qty + 1;
     setNewInventory(addedStock);
   });
-  let removeStock = useCallback((removedStock) => {
-    setChanged(true);
+  const removeStock = useCallback((removedStock) => {
     removedStock.qty = removedStock.qty - 1;
     setNewInventory(removedStock);
+  });
+  const changePrice = useCallback((newPrice, newItem) => {
+    let priceContainer;
+    if (newPrice == NaN) {
+      priceContainer = 20;
+      newItem.price = priceContainer;
+      setNewInventory(newItem);
+    } else if (newPrice == undefined) {
+      priceContainer = 20;
+      newItem.price = priceContainer;
+      setNewInventory(newItem);
+    } else {
+      priceContainer = parseInt(newPrice);
+      newItem.price = priceContainer;
+      setNewInventory(newItem);
+    }
+  });
+  const changeDiscount = useCallback((newDiscount, newItem) => {
+    let discountContainer;
+    if (newDiscount == NaN) {
+      discountContainer = 5;
+      newItem.discount = discountContainer / 100;
+      setNewInventory(newItem);
+    } else if (newPrice == undefined) {
+      discountContainer = 5;
+      newItem.price = discountContainer / 100;
+      setNewInventory(newItem);
+    } else {
+      priceContainer = parseInt(newPrice);
+      newItem.price = discountContainer / 100;
+      setNewInventory(newItem);
+    }
+  });
+  const toggleSale = useCallback((newItem) => {
+    newItem.sale = !newItem.sale;
+    setNewInventory(newItem);
   });
   return (
     <Card style={styles.container}>
@@ -24,29 +57,55 @@ export default function StockItem(props) {
         <Image source={item.image} style={styles.Image}></Image>
         <Card.Title>{item.bookName}</Card.Title>
       </View>
-      <View style={changed ? styles.changed : styles.desc}>
+      <View style={styles.desc}>
         <Text>Author: {item.author}</Text>
         <Text>Price: ${item.price}</Text>
-        <Input label="Set new price" placeholder="20"></Input>
+        <Input
+          label="Set new price"
+          placeholder="20"
+          onChangeText={(value) => setNewPrice(value)}
+        ></Input>
+        <Button
+          style={styles.inventoryButton}
+          title="Set Price"
+          onPress={() => {
+            changePrice(newPrice, item);
+          }}
+        ></Button>
         <Text>Sale: %{item.discount * 100}</Text>
-        <Input label="Set new discount" placeholder="5"></Input>
-        <CheckBox title="Is on Sale?"></CheckBox>
+        <Button
+          style={styles.inventoryButton}
+          title="Set Discount"
+          onPress={() => {
+            changeDiscount(newDiscount, item);
+          }}
+        ></Button>
+        <Input
+          label="Set new discount"
+          placeholder="5"
+          onChangeText={(value) => setNewDiscount(value)}
+        ></Input>
+        <CheckBox
+          title="Is on Sale?"
+          checked={onSale}
+          onPress={() => toggleSale(item)}
+        ></CheckBox>
         <Text>Remaining inventory: {item.qty}</Text>
+        <Button
+          style={styles.inventoryButton}
+          title="Add Inventory"
+          onPress={() => {
+            addStock(item);
+          }}
+        ></Button>
+        <Button
+          style={styles.inventoryButton}
+          title="Remove Inventory"
+          onPress={() => {
+            removeStock(item);
+          }}
+        ></Button>
       </View>
-      <Button
-        style={styles.inventoryButton}
-        title="Add Inventory"
-        onPress={() => {
-          addStock(item);
-        }}
-      ></Button>
-      <Button
-        style={styles.inventoryButton}
-        title="Remove Inventory"
-        onPress={() => {
-          removeStock(item);
-        }}
-      ></Button>
     </Card>
   );
 }
@@ -63,21 +122,9 @@ const styles = StyleSheet.create({
     height: 100,
     alignSelf: "center",
   },
-  sale: {
-    color: "red",
-  },
-  onSale: {
-    textDecorationLine: "line-through",
-    textDecorationStyle: "solid",
-  },
   desc: {
     alignItems: "center",
     fontSize: 14,
-  },
-  changed: {
-    alignItems: "center",
-    fontSize: 14,
-    color: "Green",
   },
   inventoryButton: {
     width: 100,
